d9bd7c0e63c4fe421b5c0d003636b66993e6c0a9
alfredorca <alfredorca@gmail.com>
2021-10-20 21:06:35 -0400

    Format more of the about section

--- src/Views/AboutMeView.js
+++ src/Views/AboutMeView.js
@@ -1,23 +1,24 @@
 import { Col, Container, Image, Row } from "react-bootstrap";
 import pic from "../images/unnamed-modified.png";
+import SchoolIcon from "@mui/icons-material/School";
+import CodeIcon from "@mui/icons-material/Code";
+import EmojiObjectsIcon from "@mui/icons-material/EmojiObjects";
+import GamesIcon from "@mui/icons-material/Games";
 
 const AboutMeView = () => {
   return (
     <Container fluid>
       <Row className="aboutMain">
-        <Col xl={4} lg={4} md={4} xs={12}>
+        <Col xxl={4} xl={4} lg={4} md={4} xs={12}>
           <Container>
             <div className="aboutCard">
               <div className="aboutImageDiv">
-                <Image
-                  className="img-fluid shadow-4 aboutImage"
-                  src={pic}
-                />
+                <Image className="img-fluid shadow-4 aboutImage" src={pic} />
               </div>
             </div>
           </Container>
         </Col>
-        <Col xl={8} lg={8} md={8} xs={12}>
+        <Col xxl={8} xl={8} lg={8} md={8} xs={12}>
           <Container>
             <div>
               <div className="aboutText">
@@ -40,6 +41,50 @@ const AboutMeView = () => {
                 </div>
               </div>
             </div>
+          </Container>
+        </Col>
+        <Col
+          xl={{ span: 2, offset: 1 }}
+          lg={{ span: 2, offset: 1 }}
+          md={{ span: 9, offset: 2 }}
+          xs={{ span: 9, offset: 2 }}
+        >
+          <Container className="aboutContainer">
+            <CodeIcon />
+            Experience
+          </Container>
+        </Col>
+        <Col
+          xl={{ span: 2, offset: 1 }}
+          lg={{ span: 2, offset: 1 }}
+          md={{ span: 9, offset: 2 }}
+          xs={{ span: 9, offset: 2 }}
+        >
+          <Container className="aboutContainer">
+            <SchoolIcon />
+            Always learning
+          </Container>
+        </Col>
+        <Col
+          xl={{ span: 2, offset: 1 }}
+          lg={{ span: 2, offset: 1 }}
+          md={{ span: 9, offset: 2 }}
+          xs={{ span: 9, offset: 2 }}
+        >
+          <Container className="aboutContainer">
+            <EmojiObjectsIcon />
+            Skills
+          </Container>
+        </Col>
+        <Col
+          xl={{ span: 2, offset: 1 }}
+          lg={{ span: 2, offset: 1 }}
+          md={{ span: 9, offset: 2 }}
+          xs={{ span: 9, offset: 2 }}
+        >
+          <Container className="aboutContainer">
+            <GamesIcon />
+            Hobbies
           </Container>
         </Col>
       </Row>
